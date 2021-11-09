837845d35af71e51159d7183ef8fa2c51d49ba36
github4maninder <maninder.email@gmail.com>
2021-11-09 00:09:05 -0500

    add get response for hello World

--- express_server.js
+++ express_server.js
@@ -11,6 +11,10 @@ app.get("/", (req, res) => {
   res.send("Hello!");
 });
 
+app.get("/hello", (req, res) => {
+  res.send("<html><body>Hello <b>World</b></body></html>\n");
+});
+
 app.get("/urls.json", (req, res) => {
   res.json(urlDatabase);
 });
