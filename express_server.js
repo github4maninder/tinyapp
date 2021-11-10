5bc90e130fd0a4d8c8195914e998ad9b0df63b0a
github4maninder <maninder.email@gmail.com>
2021-11-09 23:16:38 -0500

    redirect to the url'

--- express_server.js
+++ express_server.js
@@ -6,16 +6,22 @@ const bodyParser = require("body-parser");
 app.use(bodyParser.urlencoded({extended: true}));
 app.set("view engine", "ejs"); //sets the template engine we are using as ejs
 
-function generateRandomString() {
-
-}
+const generateRandomString = () => { // returns a string of 6 alphanumeric random characters
+  return Math.random().toString(36).substring(2,8);
+};
 
 const urlDatabase = {
   "b2xVn2": "http://www.lighthouselabs.ca",
   "9sm5xK": "http://www.google.com"
 };
 
+app.get("/", (req, res) => {
+  res.redirect(`/urls`);
+});
+
+//READ urls
 app.get("/urls", (req, res) => {
+  console.log('req.param', req.params);
   const templateVars = { urls: urlDatabase };
   res.render("urls_index", templateVars);
 });
@@ -34,10 +40,6 @@ app.get("/u/:shortURL", (req, res) => {
   res.redirect(longURL);
 });
 
-app.get("/", (req, res) => {
-  res.send("Hello!");
-});
-
 app.get("/urls.json", (req, res) => {
   res.json(urlDatabase);
 });
@@ -50,6 +52,17 @@ app.post("/urls", (req, res) => {
   console.log(req.body);  // Log the POST request body to the console
   res.send("Ok");         // Respond with 'Ok' (we will replace this)
 });
+
+app.post("/urls/:shortURL",(req,res) => {
+  const shortURLToEdit = req.params.shortURL;
+  res.redirect(`/urls/${shortURLToEdit}`);
+})
+
+app.post("/urls/:shortURL/delete", (req,res) => {
+  const shortURLToDel = req.params.shortURL;
+  delete urlDatabase[shortURLToDel];
+  res.redirect('/urls');
+})
 
 app.get("/set", (req, res) => {
   const a = 1;
