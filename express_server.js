0917c05184010b7591bbb3e97d87d30b84015233
github4maninder <maninder.email@gmail.com>
2021-11-10 18:38:24 -0500

    update cookie feature

--- express_server.js
+++ express_server.js
@@ -30,14 +30,31 @@ app.get("/", (req, res) => {
 //READ for all urls
 app.get("/urls", (req, res) => {
   console.log('req.param', req.params);
-  const templateVars = { urls: urlDatabase };
+  let templateVars = {
+    username: req.cookies["username"],
+    urls: urlDatabase
+  };
   res.render("urls_index", templateVars);
 });
 
 //READ a form to submit a new URL
 app.get("/urls/new", (req, res) => {
   console.log('req.param', req.params);
-  res.render("urls_new");
+  let templateVars = {
+    username: req.cookies["username"]
+  };
+  res.render("urls_new", templateVars);
+});
+
+//READ a new shortURL link after form submission
+app.get("/urls/:shortURL", (req, res) => {
+  console.log('req.param', req.params);
+  const templateVars = { 
+    username: req.cookies['username'],
+    shortURL: req.params.shortURL, 
+    longURL: urlDatabase[req.params.shortURL] };
+  res.render("urls_show", templateVars);
+  res.redirect("/urls");
 });
 
 //EDIT create a new shortURL after form submission, saves to urlDataBase and redirect
@@ -49,16 +66,25 @@ app.post("/urls", (req,res) => {
   res.redirect(`/urls/${shortURL}`);
 });
 
-//READ a new shortURL link after form submission
-app.get("/urls/:shortURL", (req, res) => {
-  console.log('req.param', req.params);
-  const templateVars = { shortURL: req.params.shortURL, longURL: urlDatabase[req.params.shortURL] };
-  res.render("urls_show", templateVars);
+
+
+// EDIT login page
+app.post('/login', (req, res) => {
+  const username = req.body.username;
+  console.log("username", username);
+  res.cookie("username", username);
+  res.redirect("/urls");
+});
+
+app.post('/logout', (req, res) => {
+  res.clearCookie('username');
+  res.redirect('/urls');
 });
 
 // redirect after submit 
 app.post("/urls/:shortURL", (req, res) => {
   const shortURL = req.params.shortURL;
+  urlDatabase[shortURL] = req.body.longURL;
   res.redirect(shortURL);
 })
 
@@ -70,55 +96,12 @@ app.get("/u/:shortURL", (req, res) => {
   res.redirect(longURL);
 });
 
-// register page
-app.get('/register', (req, res) => {
-  const { user_id } = req.session;
-  const user = usersDb[user_id];
-  let templateVars = {user};
-  res.render('urls_form', templateVars);
-});
-
-// login page
-app.post('/login', (req, res) => {
-  const { email, password } = req.body;
-  const userFound = getUserByEmail(email, usersDb);
-  const error = 'Error 403: Please check your email and password and try again';
-  if (userFound && checkPasswords(password, userFound)) {
-    req.session.user_id = userFound.id;
-    res.redirect('/urls');
-  } else {
-    res.send(error);
-  }
-});
-
 //DELETE a single URL
 app.post("/urls/:shortURL/delete", (req,res) => {
   const shortURLToDel = req.params.shortURL;
   delete urlDatabase[shortURLToDel]; // delete the property in urlDatabase obj
   res.redirect('/urls');
 })
-
-// Logout page
-app.post('/logout',(req, res) =>{
-  req.session.user_id = '';
-  res.redirect('/urls');
-})
-
-// register page
-app.post('/register', (req, res) => {
-  const id = generateRandomString();
-  const { email, password } = req.body;
-  const hashedPassword = bcrypt.hashSync(password, 10);
-  const user = getUserByEmail(email, usersDb);
-  const validRegistration = validateRegistration(email, password, user);
-  if (validRegistration) {
-    usersDb[id] = {id, email, hashedPassword};
-    req.session.user_id = id;
-    res.redirect('/urls');
-  } else {
-    res.send('Error 400: Please check the details you entered and try again!');
-  }
-});
 
 app.listen(PORT, () => {
   console.log(`TinyApp server listening on port ${PORT}!`);
