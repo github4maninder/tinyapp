eb498ef9751ac7e3fd799a8d25f9f1d0b3edb8ac
github4maninder <maninder.email@gmail.com>
2021-11-11 23:09:33 -0500

    refactor code

--- express_server.js
+++ express_server.js
@@ -51,6 +51,89 @@ const users = {
   }
 };
 
+// redirects to /urls if logged in, or /login if not logged in
+app.get("/", (req, res) => {
+  if (req.session.user_id) {
+    res.redirect("/urls");
+  } else {
+    res.redirect("/login");
+  }
+});
+
+//renders URLs page with list of all the URLs currently in the database
+app.get("/urls", (req, res) => {
+  const templateVars = { urls: urlsForUser(req.session.user_id, urlDatabase), user: users[req.session.user_id] };
+  res.render("urls_index", templateVars);
+});
+
+//renders new URL page
+app.get("/urls/new", (req, res) => {
+  if (req.session.user_id) {
+    res.render("urls_new", { user: users[req.session.user_id] });
+  } else {
+    res.redirect("/login");
+  }
+});
+
+//renders individual page for a URL
+app.get("/urls/:id", (req, res) => {
+  if (!urlDatabase[req.params.id]) {
+    return res.send("<html><h1>Error: this URL does not exist</h1></html> ");
+  } else if (!req.session.user_id) {
+    return res.send("<html><h1>Error: please login to view this page</h1></html> ");
+  } else if (req.session.user_id !== urlDatabase[req.params.id].userID) {
+    return res.send('<html><h1>Error: you do not have permission to view this URL</h1></html>');
+  }
+  const templateVars = { shortURL: req.params.id, longURL: urlDatabase[req.params.id].longURL, user: users[req.session.user_id] };
+  res.render("urls_show", templateVars);
+});
+
+//redirects to the actual page when user clicks on short url link
+app.get("/u/:id", (req, res) => {
+  const urlID = urlDatabase[req.params.id];
+  if (urlID === undefined) {
+    res.statusCode = 404;
+    return res.send("<html><h1>Error: shortURL does not exist</h1></html>");
+  } else {
+    res.redirect(urlID.longURL);
+  }
+});
+
+//generates a new shortURL, adds it to the database, and redirects to the "show" page
+app.post("/urls", (req, res) => {
+  if (!req.session.user_id) {
+    return res.send('<html><h1>Error: please login to view this page</h1></html>');
+  }
+  let tempString = generateRandomString();
+  urlDatabase[tempString] = {
+    longURL: req.body.longURL,
+    userID: req.session.user_id
+  };
+  return res.redirect(`/urls/${tempString}`);
+});
+
+
+//EDIT URL: updates an existing url in the database, and redirects back to URLs page
+app.post("/urls/:id", (req, res) => {
+  if (req.session.user_id && req.session.user_id === urlDatabase[req.params.id].userID) {
+    urlDatabase[req.params.id].longURL = req.body.longURL;
+    res.redirect("/urls");
+  } else {
+    return res.send("<html><h1>Error: you do not have permission to edit this URL</h1></html>");
+  }
+});
+
+//DELETE URL: removes a URL resource and redirects back to URLs page
+app.post("/urls/:id/delete", (req, res) => {
+  console.log("req.session", req.session);
+  if (req.session.user_id && req.session.user_id === urlDatabase[req.params.id].userID) {
+    delete urlDatabase[req.params.id];
+    return res.redirect("/urls");
+  }
+  return res.send("<html><h1>Error: you do not have permission to delete this URL</h1></html>");
+});
+
+
 app.get("/login", (req, res) => {
   if (req.session.user_id) {
     return res.redirect("/urls");
@@ -106,91 +189,6 @@ app.post("/register", (req, res) => {
 app.post("/logout", (req, res) => {
   delete req.session.user_id;
   res.redirect("/urls");
-});
-
-//redirects to the actual page when user clicks on short url link
-app.get("/u/:id", (req, res) => {
-  const urlID = urlDatabase[req.params.id];
-  if (urlID === undefined) {
-    res.statusCode = 404;
-    return res.send("<html><h1>Error: shortURL does not exist</h1></html>");
-  } else {
-    res.redirect(urlID.longURL);
-  }
-});
-
-//renders new URL page
-app.get("/urls/new", (req, res) => {
-  if (req.session.user_id) {
-    res.render("urls_new", { user: users[req.session.user_id] });
-  } else {
-    res.redirect("/login");
-  }
-});
-
-//DELETE URL: removes a URL resource and redirects back to URLs page
-app.post("/urls/:id/delete", (req, res) => {
-  console.log("req.session", req.session);
-  if (req.session.user_id && req.session.user_id === urlDatabase[req.params.id].userID) {
-    delete urlDatabase[req.params.id];
-    return res.redirect("/urls");
-  }
-  return res.send("<html><h1>Error: you do not have permission to delete this URL</h1></html>");
-});
-
-//EDIT URL: updates an existing url in the database, and redirects back to URLs page
-app.post("/urls/:id", (req, res) => {
-  if (req.session.user_id && req.session.user_id === urlDatabase[req.params.id].userID) {
-    urlDatabase[req.params.id].longURL = req.body.longURL;
-    res.redirect("/urls");
-  } else {
-    return res.send("<html><h1>Error: you do not have permission to edit this URL</h1></html>");
-  }
-});
-
-//renders individual page for a URL
-app.get("/urls/:id", (req, res) => {
-  if (!urlDatabase[req.params.id]) {
-    return res.send("<html><h1>Error: this URL does not exist</h1></html> ");
-  } else if (!req.session.user_id) {
-    return res.send("<html><h1>Error: please login to view this page</h1></html> ");
-  } else if (req.session.user_id !== urlDatabase[req.params.id].userID) {
-    return res.send('<html><h1>Error: you do not have permission to view this URL</h1></html>');
-  }
-  const templateVars = { shortURL: req.params.id, longURL: urlDatabase[req.params.id].longURL, user: users[req.session.user_id] };
-  res.render("urls_show", templateVars);
-});
-
-//renders URLs page with list of all the URLs currently in the database
-app.get("/urls", (req, res) => {
-  const templateVars = { urls: urlsForUser(req.session.user_id, urlDatabase), user: users[req.session.user_id] };
-  res.render("urls_index", templateVars);
-});
-
-//generates a new shortURL, adds it to the database, and redirects to the "show" page
-app.post("/urls", (req, res) => {
-  if (!req.session.user_id) {
-    return res.send('<html><h1>Error: please login to view this page</h1></html>');
-  }
-  let tempString = generateRandomString();
-  urlDatabase[tempString] = {
-    longURL: req.body.longURL,
-    userID: req.session.user_id
-  };
-  return res.redirect(`/urls/${tempString}`);
-});
-
-// redirects to /urls if logged in, or /login if not logged in
-app.get("/", (req, res) => {
-  if (req.session.user_id) {
-    res.redirect("/urls");
-  } else {
-    res.redirect("/login");
-  }
-});
-
-app.get("/urls.json", (req, res) => {
-  res.json(urlDatabase);
 });
 
 // LISTENER
